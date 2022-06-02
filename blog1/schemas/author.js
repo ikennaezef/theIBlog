d32dbbd19d331cd8975720bd75f82b3b9b671705
Ikenna <iykee0333@gmail.com>
2022-06-02 17:10:19 +0100

    created post schema and added posts

--- blog1/schemas/author.js
+++ blog1/schemas/author.js
@@ -1,30 +1,30 @@
 export default {
-  name: "author",
-  title: "Author",
-  type: "document",
+  name: 'author',
+  title: 'Author',
+  type: 'document',
   fields: [
     {
-      name: "name",
-      title: "Author's Name",
-      type: "string",
+      name: 'name',
+      title: 'Author\'s Name',
+      type: 'string',
     },
     {
-      name: "image",
-      title: "Image",
-      type: "image",
+      name: 'image',
+      title: 'Image',
+      type: 'image',
       options: {
         hotspot: true,
       }
     },
     {
-      name: "bio",
-      title: "Bio",
-      type: "array",
+      name: 'bio',
+      title: 'Bio',
+      type: 'array',
       of: [
         {
-          title: "Block",
-          type: "block",
-          styles: [{ title: "Normal", value: "normal" }],
+          title: 'Block',
+          type: 'block',
+          styles: [{ title: 'Normal', value: 'normal' }],
           lists: []
         }
       ]
---
+++ blog1/schemas/post.js
@@ -0,0 +1,52 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'string'
+    },
+    {
+      name: 'publicationDate',
+      title: 'Date of Publication',
+      type: 'date',
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: [{ type: 'author' }]
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'string' }]
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image'
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{ title: 'Normal', value: 'normal' }],
+          lists: []
+        }
+      ]
+    }
+  ]
+}
