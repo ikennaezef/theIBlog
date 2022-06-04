108709287fb09fdbbec7dbf6156c6bd55d84fbe8
Ikenna <iykee0333@gmail.com>
2022-06-04 06:30:04 +0100

    meta

---
+++ components/Meta.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const Meta = ({ title, description }) => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name='keywords' content='news, information, blog' />
+      <meta name='description' content={description} />
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'TheIBlog',
+  description: 'A community of readers looking to share in each others experience and opinions.'
+}
+
+export default Meta
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import Nav from '../components/Nav'
+import Meta from '../components/Meta'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Meta />
       <Nav />
       <Component {...pageProps} />
     </>
